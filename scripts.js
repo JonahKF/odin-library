b02095edf88c34333360b2ded23ddb2a3a7bb622
Jonah <jonahkf@gmail.com>
2024-10-09 16:42:55 +0900

    INITIAL: Set up HTML, CSS, JS files for project.

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function Book(title, author, pageCount, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+    this.info = function() {
+        if(this.readStatus === true) {
+            return `${this.title} by ${this.author}, ${this.pageCount} pages, already read.`
+        }
+        else if(this.readStatus === false) {
+            return `${this.title} by ${this.author}, ${this.pageCount} pages, not yet read.`
+        }
+    }
+}
+
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
